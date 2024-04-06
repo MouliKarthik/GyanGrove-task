50e07a34fda9ee1d1a373ccabf10bbcbbf21e12e
MouliKarthik <105270064+MouliKarthik@users.noreply.github.com>
2024-04-06 19:12:25 +0530

    added cors

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const responsesInPages = require('./utils/responseInPages');
 
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next(); 
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
